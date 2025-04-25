cdf5530fdc167f6835a3540680d787e93aabba42
TimOthry <timsonlee3@gmail.com>
2025-04-25 10:56:15 +0100

    Modified config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     reactStrictMode: true,
-    output: 'export'
+    output: 'export',
+    trailingSlash: true, // Optional: Adds trailing slashes to URLs
+    exportPathMap: async function () {
+        return {
+        '/': { page: '/' },
+        '/works': { page: '/works' },
+        '/works/alptrader': { page: '/works/alptrader' },
+        }
+    }
 }
