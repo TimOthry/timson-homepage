647e8f00604b5fddf6227d76712082fe037a9ec8
TimOthry <timsonlee3@gmail.com>
2025-04-27 14:09:16 +0100

    Added favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -14,6 +14,7 @@ const Main = ({ children, router }) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" type="image/svg+xml" href="/timson-homepage/images/favicon.svg" />
                 <title>Timson Lee | Homepage</title>
             </Head>
 
